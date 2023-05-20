7c49bb89065437b0241604d708c8af3a7e8f1db0
leemon <tacrapo@gmail.com>
2023-05-20 12:49:02 +0200

    Add zoom buttons in the editor

--- build/blocks/panzoom/index.asset.php
+++ build/blocks/panzoom/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3bff2e2be4a294f9b739');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'aa18da552410858029e2');
--- src/blocks/panzoom/index.js
+++ src/blocks/panzoom/index.js
@@ -87,6 +87,10 @@ export const settings = {
 										alt={alt}
 									/>
 								</div>
+								<div className="panzoom-buttons">
+									<button className="zoomin-button"><span className="screen-reader-text">Zoom In</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg></button>
+									<button className="zoomout-button"><span className="screen-reader-text">Zoom Out</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,13H5V11H19V13Z" /></svg></button>
+								</div>
 							</div>
 						}
 						{!url &&
