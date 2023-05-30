bb36b63971646e446077076cb1338a84a0d34c9b
leemon <tacrapo@gmail.com>
2023-05-30 11:29:06 +0200

    i18n strings

--- build/blocks/panzoom/index.asset.php
+++ build/blocks/panzoom/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'aa18da552410858029e2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '174220b0fe9a44668055');
--- src/blocks/panzoom/index.js
+++ src/blocks/panzoom/index.js
@@ -88,8 +88,14 @@ export const settings = {
 									/>
 								</div>
 								<div className="panzoom-buttons">
-									<button className="zoomin-button"><span className="screen-reader-text">Zoom In</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg></button>
-									<button className="zoomout-button"><span className="screen-reader-text">Zoom Out</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,13H5V11H19V13Z" /></svg></button>
+									<button className="zoomin-button">
+										<span className="screen-reader-text">{__('Zoom In', 'rather-simple-panzoom')}</span>
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg>
+									</button>
+									<button className="zoomout-button">
+										<span className="screen-reader-text">{__('Zoom Out', 'rather-simple-panzoom')}</span>
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,13H5V11H19V13Z" /></svg>
+									</button>
 								</div>
 							</div>
 						}
@@ -130,8 +136,14 @@ export const settings = {
 						/>
 					</div>
 					<div className="panzoom-buttons">
-						<button className="zoomin-button"><span className="screen-reader-text">Zoom In</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg></button>
-						<button className="zoomout-button"><span className="screen-reader-text">Zoom Out</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,13H5V11H19V13Z" /></svg></button>
+						<button className="zoomin-button">
+							<span className="screen-reader-text">Zoom In</span>
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg>
+						</button>
+						<button className="zoomout-button">
+							<span className="screen-reader-text">Zoom Out</span>
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,13H5V11H19V13Z" /></svg>
+						</button>
 					</div>
 				</div>
 			</div>
