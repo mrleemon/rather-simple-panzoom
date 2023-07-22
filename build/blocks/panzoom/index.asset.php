5ae2db70049ab4f1a1760a05e9eb971f5ea95b98
leemon <tacrapo@gmail.com>
2023-07-23 00:14:09 +0200

    Set color to block icon

--- build/blocks/panzoom/index.asset.php
+++ build/blocks/panzoom/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '89c5a181a7ac54e0846f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '02d05124fa0fe2caa9ee');
