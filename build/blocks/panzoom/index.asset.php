7208c217edc2fe11590a03b17e2ae6f162b87457
leemon <tacrapo@gmail.com>
2023-08-14 20:29:54 +0200

    Replace useEffect with useRefEffect to make the Panzoom lib work in iframed editors

--- build/blocks/panzoom/index.asset.php
+++ build/blocks/panzoom/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3fa59fdbd7321c912e60');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'c81397ee40b4f9dea7ec');
--- src/blocks/panzoom/index.js
+++ src/blocks/panzoom/index.js
@@ -15,7 +15,13 @@ import {
 	MediaReplaceFlow,
 	useBlockProps
 } from '@wordpress/block-editor';
-import { useRef, useEffect } from '@wordpress/element';
+import {
+	useRef,
+	useEffect
+} from '@wordpress/element';
+import {
+	useRefEffect
+} from '@wordpress/compose';
 import { registerBlockType } from '@wordpress/blocks';
 
 /**
@@ -30,17 +36,17 @@ import './style.scss';
 const { name } = metadata;
 
 export const settings = {
-	
+
 	icon: {
 		src: <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 			<Path d="M15.5,2C18,2 20,4 20,6.5C20,7.38 19.75,8.21 19.31,8.9L22.39,12L21,13.39L17.88,10.32C17.19,10.75 16.37,11 15.5,11C13,11 11,9 11,6.5C11,4 13,2 15.5,2M15.5,4A2.5,2.5 0 0,0 13,6.5A2.5,2.5 0 0,0 15.5,9A2.5,2.5 0 0,0 18,6.5A2.5,2.5 0 0,0 15.5,4M7.5,14.5L4,19H18L13.5,13L10,17.5L7.5,14.5M20,20A2,2 0 0,1 18,22H4A2,2 0 0,1 2,20V6A2,2 0 0,1 4,4H9.5C9.18,4.77 9,5.61 9,6.5A6.5,6.5 0 0,0 15.5,13C16.18,13 16.84,12.89 17.46,12.7L20,15.24V20Z" />
-			</SVG>,
+		</SVG>,
 		foreground: '#ff8a00'
 	},
-	
+
 	edit: (props) => {
 		const blockProps = useBlockProps();
-		const ref = useRef();
+		//const ref = useRef();
 		const {
 			attributes: { url, id, alt },
 			setAttributes,
@@ -50,7 +56,7 @@ export const settings = {
 			setAttributes({ url: media.url, id: media.id, alt: media.alt });
 		};
 
-		useEffect( () => {
+		/*useEffect( () => {
 			if ( ref.current ) {
 				// The Panzoom constructor is called when a new block is added and an image is selected/replaced.
 				var panzoom = Panzoom(ref.current, {
@@ -70,6 +76,34 @@ export const settings = {
 
 				ref.current.parentElement.addEventListener('wheel', panzoom.zoomWithWheel);
 			}
+
+			return () => {
+				if (panzoom) {
+					// Destroy Panzoom instance after every render.
+					panzoom.destroy();
+				}
+			}
+
+		}, [url]);*/
+
+		const ref = useRefEffect((element) => {
+			// The Panzoom constructor is called when a new block is added and an image is selected/replaced.
+			var panzoom = Panzoom(element, {
+				minScale: 1.1,
+				maxScale: 5,
+				startScale: 1.1,
+				animate: true,
+				duration: 1000,
+				origin: '50% 0'
+			});
+
+			var zoomInButton = element.parentElement.querySelector('.zoomin-button');
+			var zoomOutButton = element.parentElement.querySelector('.zoomout-button');
+
+			zoomInButton.addEventListener('click', panzoom.zoomIn);
+			zoomOutButton.addEventListener('click', panzoom.zoomOut);
+
+			element.parentElement.addEventListener('wheel', panzoom.zoomWithWheel);
 
 			return () => {
 				if (panzoom) {
