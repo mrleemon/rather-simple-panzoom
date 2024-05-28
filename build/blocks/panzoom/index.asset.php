931cbe6a2488b41d8b3ed1541419254758d9c13e
leemon <tacrapo@gmail.com>
2024-05-28 16:48:51 +0200

    Rebuild

--- build/blocks/panzoom/index.asset.php
+++ build/blocks/panzoom/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'c81397ee40b4f9dea7ec');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '6e6388ed57d7ed4acb4f');
